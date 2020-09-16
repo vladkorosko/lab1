d972b93e366c99dd7dd875c3d761e5e12940b43d
Владислав Коросько <vlad.korosko1@gmail.com>
2020-09-16 21:13:49 +0300

    Added Change TimeZone for Date and SetTime for Time

--- lab1/lab1.cpp
+++ lab1/lab1.cpp
@@ -5,7 +5,8 @@ int main()
     srand(time(NULL));
     try
     {
-        Date a("+2: 2020.10.18; 10:00:30");
+        Date a("+12: 1970.01.1; 10:00:30");
+        a.ChangeUTC(-12);
         cout << a;
     }
     catch (logic_error e)
