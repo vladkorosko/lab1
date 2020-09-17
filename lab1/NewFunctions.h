0f125137281f02315e31c824b983bc6268e18d10
Владислав Коросько <vlad.korosko1@gmail.com>
2020-09-17 11:49:47 +0300

    Overloaded operators <, == and > for Date

--- lab1/NewFunctions.h
+++ lab1/NewFunctions.h
@@ -27,7 +27,7 @@ ostream& operator<<(ostream& s, Date date)
 	return s;
 }
 
-bool operator>(Time t1, Time t2)
+bool operator>(const Time& t1, const Time& t2)
 {
 	if (t1.GetHour() > t2.GetHour())
 		return true;
@@ -38,18 +38,58 @@ bool operator>(Time t1, Time t2)
 	return false;
 }
 
-bool operator==(Time t1, Time t2)
+bool operator==(const Time& t1, const Time& t2)
 {
 	return t1.GetTime() == t2.GetTime();
 }
 
-bool operator<(Time t1, Time t2)
+bool operator<(const Time& t1, const Time& t2)
 {
 	if (t1.GetHour() < t2.GetHour())
 		return true;
 	else if (t1.GetHour() == t2.GetHour() && t1.GetMinute() < t2.GetMinute())
 		return true;
 	else if (t1.GetHour() == t2.GetHour() && t1.GetMinute() == t2.GetMinute() && t1.GetSecond() < t2.GetSecond())
+		return true;
+	return false;
+}
+
+bool operator>(Date d1, Date d2)
+{
+	if (d1.GetUTC() > d2.GetUTC())
+		d2.ChangeUTC(d1.GetUTC());
+	else d1.ChangeUTC(d2.GetUTC());
+	if (d1.GetYear() > d2.GetYear())
+		return true;
+	else if (d1.GetYear() == d2.GetYear() && d1.GetMonth() > d2.GetMonth())
+		return true;
+	else if (d1.GetYear() == d2.GetYear() && d1.GetMonth() == d2.GetMonth() && d1.GetDay() > d2.GetDay())
+		return true;
+	else if (d1.GetYear() == d2.GetYear() && d1.GetMonth() == d2.GetMonth() && d1.GetDay() == d2.GetDay() && d1.GetTime() > d2.GetTime())
+		return true;
+	return false;
+}
+
+bool operator==(Date d1, Date d2)
+{
+	if (d1.GetUTC() > d2.GetUTC())
+		d2.ChangeUTC(d1.GetUTC());
+	else d1.ChangeUTC(d2.GetUTC());
+	return d1.GetDate() == d2.GetDate();
+}
+
+bool operator<(Date d1, Date d2)
+{
+	if (d1.GetUTC() > d2.GetUTC())
+		d2.ChangeUTC(d1.GetUTC());
+	else d1.ChangeUTC(d2.GetUTC());
+	if (d1.GetYear() < d2.GetYear())
+		return true;
+	else if (d1.GetYear() == d2.GetYear() && d1.GetMonth() < d2.GetMonth())
+		return true;
+	else if (d1.GetYear() == d2.GetYear() && d1.GetMonth() == d2.GetMonth() && d1.GetDay() < d2.GetDay())
+		return true;
+	else if (d1.GetYear() == d2.GetYear() && d1.GetMonth() == d2.GetMonth() && d1.GetDay() == d2.GetDay() && d1.GetTime() < d2.GetTime())
 		return true;
 	return false;
 }
